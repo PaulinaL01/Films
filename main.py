565b07a340e99f5cbf434f6cb6549784547acc6c
Michalina <krysciakmichalina1@gmail.comj>
2021-05-29 23:44:22 +0200

    Popular movies

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template, request
+import requests
+import json
 
-a = int(2)
-b= 2
+movies = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=d086e02925aea6ae99f8b04207381382&language=en-US&page=1").json()
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html",movies=movies)
+
+app.run(debug=True)
