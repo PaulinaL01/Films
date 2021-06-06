5ed45ac4c34d62af08d88d46769e25b7be6dbeb7
Michalina <krysciakmichalina1@gmail.comj>
2021-06-06 23:27:50 +0200

    Popular movies image

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import requests
 import json
 
 movies = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=d086e02925aea6ae99f8b04207381382&language=en-US&page=1").json()
-
+link = "https://api.themoviedb.org/3/movie/popular?api_key=d086e02925aea6ae99f8b04207381382&language=en-US&page=1"
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html",movies=movies)
+    return render_template("home.html", movies=movies, link=link)
 
 app.run(debug=True)
