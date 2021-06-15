167a8320041d875feee1c2975e35b070f242dd78
Michalina <krysciakmichalina1@gmail.comj>
2021-06-15 15:29:49 +0200

    15.06.

--- website/__init__.py
+++ website/__init__.py
@@ -3,11 +3,10 @@ from flask import Flask, render_template
 from website.movies import Movies
 
 app = Flask(__name__)
+movies = Movies()
 
 
 @app.route("/")
 def home():
-    title = Movies().getMovies()
-    poster = Movies().getMovies()
-    return render_template("home.html", title= title, poster= poster)
+    return render_template("home.html", movies=movies.getPopular())
 
--- website/movies.py
+++ website/movies.py
@@ -6,9 +6,13 @@ class Movies:
         self.all_movies = requests.get(
             "https://api.themoviedb.org/3/movie/popular?api_key=d086e02925aea6ae99f8b04207381382&language=en-US&page=1").json()
 
-    def getMovies(self):
-        for movie in self.all_movies['results']:
+    def getPopular(self):
+        res = []
+        for i, movie in enumerate(self.all_movies['results']):
+            if i % 4 == 0:
+                current_list = []
+                res.append(current_list)
             title = movie['original_title']
             poster = 'https://image.tmdb.org/t/p/w400'+movie['poster_path']
-
-            return title, poster
+            current_list.append({"title" : title, "poster" : poster})
+        return res
