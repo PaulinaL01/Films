449858842aa7739f163a22912cfc5d577f9ee28f
Michalina <krysciakmichalina1@gmail.comj>
2021-07-04 08:24:42 +0200

    nie działa pętla w int która ma pokazywać pokolei aktorów (pokazuje jedego)

--- website/__init__.py
+++ website/__init__.py
@@ -20,11 +20,32 @@ def home():
 
 @app.route("/overview/<int:id>")
 def overview(id):
-    print(id)
-    # if request.method == "POST":
-    #     selected = request.args.get('movie.title')
-    #     print("selected:", selected)
-    #     selectedInfo = ""
-    #     if selected is not None:
-    #         selectedInfo = "hello"
-    return render_template("overview.html", movies=movies.getPopular())
+    id = str(id)
+    link_overview = 'https://api.themoviedb.org/3/movie/'+ id + "?api_key=d086e02925aea6ae99f8b04207381382"
+    link_overview = requests.get(link_overview).json()
+    print(link_overview)
+    pos='https://image.tmdb.org/t/p/w400'+link_overview['poster_path']
+    credits = 'https://api.themoviedb.org/3/movie/' + id + "/credits?api_key=d086e02925aea6ae99f8b04207381382"
+    all_credits = requests.get(credits).json()
+
+
+    full_cast = all_credits['cast']
+    cast_length = len(full_cast)
+    print(full_cast,cast_length)
+
+
+    #
+    # res = []
+    # for i, movie in range(cast_length):
+    #     if i % 3 == 0:
+    #         current_list = []
+    #         res.append(current_list)
+    #     actor_name = movie['name']
+    #     actor_role = movie['character']
+    #     actor_pic = 'https://image.tmdb.org/t/p/w400' + movie['profile_path']
+    #     current_list.append({"actor_name": actor_name, "actor_role": actor_role, "actor_pic": actor_pic})
+    #     return res
+
+
+
+    return render_template("overview.html", movies=movies.getPopular(), link_overview=link_overview, pos=pos, all_credits=all_credits, full_cast=full_cast)
